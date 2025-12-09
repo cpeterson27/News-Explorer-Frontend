fc9ab8685ff11c9a860027433c87cd1989280fef
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-12-08 18:21:44 -0600

    change to https

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const BASE_URL = import.meta.env.PROD
-  ? 'http://api.newsexplorer27.mooo.com'
+  ? 'https://api.newsexplorer27.mooo.com'
   : 'http://localhost:5001';
 
 const apiKey = '78de188aac524be7b98351628b677b25';
