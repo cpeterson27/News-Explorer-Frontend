e6c1e48a87477a1e82c26989f5f257d19d5a76ce
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-12-14 19:46:57 -0600

    update base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
-const BASE_URL = 'https://api.newsexplorer28.crabdance.com';
+const BASE_URL = process.env.NODE_ENV === "production"  
+? 'https://api.newsexplorer28.crabdance.com/api/news'
+: "http://localhost:5001";
 
 export { BASE_URL };
