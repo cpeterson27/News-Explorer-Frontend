4096abe4d90c63f1d92891037273b736a65810f9
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-12-21 12:07:23 -0600

    update auth checkToken and login function

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -13,6 +13,23 @@ export const register = (name, email, password) => {
   }).then(handleServerResponse);
 };
 
-export const login = (email, password) => fetch(`${BASE_URL}/api/auth/signin`, {method: 'POST', headers: { Accept: 'application/json', 'Content-Type': 'application/json' }, body: JSON.stringify({ email, password }) }).then(handleServerResponse);
+export const login = (email, password) => {
+  return fetch(`${BASE_URL}/api/auth/signin`, {
+    method: 'POST', 
+    headers: { 
+      Accept: 'application/json', 
+      'Content-Type': 'application/json' 
+    }, 
+    body: JSON.stringify({ email, password }) 
+  }).then(handleServerResponse);
+};
 
-export const checkToken = (token) => fetch(`${BASE_URL}/api/auth/user`, { method: 'GET', headers: { Accept: 'application/json', 'Content-Type': 'application/json', authorization: `Bearer ${token}` } }).then(handleServerResponse);
+export const checkToken = (token) => {
+  return fetch(`${BASE_URL}/api/auth/user`, { 
+    method: 'GET', 
+    headers: { 
+      Accept: 'application/json', 
+      'Content-Type': 'application/json', 
+      authorization: `Bearer ${token}` } 
+    }).then(handleServerResponse);
+  };
