a4c542abe0875a7e9b9429a3053f0f551e731676
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2026-01-05 22:03:51 -0600

    config url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
-const BASE_URL = ""
+const BASE_URL = window.location.hostname === 'localhost'    ? 
+'http://localhost:5000'    : 
+'https://api.newsexplorer28.crabdance.com';  
 
 export { BASE_URL };
